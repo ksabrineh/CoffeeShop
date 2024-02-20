8503629fb49270d5416be9b6139d2de9cacb1f46
Sabrineh <ksabrineh@gmail.com>
2024-02-20 23:21:20 +0330

    fix folders

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Menu from "../../components/Menu";
+import LandingPage from "../../components/LandingPage";
+
+export default function Home() {
+  return (
+    <>
+      <Menu />
+      <LandingPage />
+    </>
+  );
+}
