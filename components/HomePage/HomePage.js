a65819cff90f3d23616660aa5d1e177593033497
Sabrineh <ksabrineh@gmail.com>
2024-02-22 12:27:57 +0330

    sabrineh: fix next and tailwind config 2024/02/22 12:27

---
+++ components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuBarPage from "../MenuBar/MenuBarPage";
+
+const HomePage = () => {
+  return (
+    <>
+      <MenuBarPage />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ components/MenuBar/MenuBarPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MenuBarPage = () => {
+    return (
+        <div className="flex justify-between p-5 w-full items-center">
+            <div className="flex justify-between w-[50%] items-center">
+                <div>menu</div>
+                <div>menu</div>
+                <div>menu</div>
+                <div>menu</div>
+            </div>
+            <div className="flex w-[50%] justify-end">
+                logo
+            </div>
+        </div>
+    );
+};
+
+export default MenuBarPage;
